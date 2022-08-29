d54bb4b4dcd28d8c975c95cbf6290f2262d67217
feddericahu99 <federica.hu339@gmail.com>
2022-08-29 15:34:28 +0200

    $GET

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php 
 
-$text= 'Giuseppe ha maangiato tutte le mele! 
+$text= 'Giuseppe ha mangiato tutte le mele comprate al supermercato! 
 dovremmo passare al supermercato a prendere delle
-mele per fare la torta alle mele.';
+mele per fare la torta alle mele a Giuseppe.';
 
+//text & length
 echo $text;
-echo '<br> <br/>';
+echo '<br> </br>';
 echo strlen($text);
 
 //query
+$bad_word = $_GET['word'];
 
-$bad_word = $_GET['mele'];
-echo $bad_word; 
+//replace
+$new_text= str_replace($bad_word, '***', $text);
+
+//new text
+echo '<br> </br>';
+echo $new_text;
+echo '<br> </br>';
+echo strlen($new_text);
 
 ?>
