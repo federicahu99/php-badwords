d0b43275bd1760818205327617bc0ec2ec33721a
feddericahu99 <federica.hu339@gmail.com>
2022-08-29 14:52:16 +0200

    created php file added text

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+$text= 'Giuseppe ha maangiato tutte le mele! 
+dovremmo passare al supermercato a prendere delle
+mele per fare la torta alle mele.';
+
+echo $text;
+echo '<br> <br/>';
+echo strlen($text);
+
+?>
